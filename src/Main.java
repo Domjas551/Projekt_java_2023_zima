c2a13aa853ee63bcb8ff5a78d2008488e156c2fc
Domjas551 <domjas@onet.pl>
2023-12-01 13:37:54 +0100

    Added DB server functionality

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import sk.*;
+
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.*;
 
 import java.io.*;
@@ -9,8 +12,44 @@ import java.util.Scanner;
 
 import static java.time.LocalDateTime.*;
 
+class Launcher {
+    public Launcher() {
+        try {
+            handleConnection();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void handleConnection() throws IOException {
+        ServerSocket ss = new ServerSocket(6700);
+
+        while (true) {
+            System.out.println("ps");
+            Socket socket = ss.accept();
+            System.out.println("pos");
+            Sesja o = new Sesja(socket);
+            Thread t = new Thread(o);
+            t.start();
+//            PrintWriter pw=new PrintWriter(socket.getOutputStream(), true);
+//            InputStreamReader isr=new InputStreamReader(socket.getInputStream());
+//            BufferedReader br=new BufferedReader(isr);
+//
+//            pw.println("Połączenie z serwerem zostało nawiązane.");
+//            System.out.println("przesłanie");
+//            System.out.println(br.readLine());
+//            System.out.println("odczytanie");
+//
+//            pw.close();
+        }
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
+
+        new Launcher();
+
         /*
         //Zmiany
         //Usunięcie
@@ -129,7 +168,7 @@ public class Main {
 */
         //Testy BD
         //trzeba w File>Project Structure>Modules>Dependencies dodaj odpowiedni .jar z driverem (oracle jdbc driver)
-        try{
+        /*try{
             //Krok 1: załaduj klase drivera
             Class.forName("oracle.jdbc.driver.OracleDriver");
 
@@ -144,22 +183,28 @@ public class Main {
             //Krok 3: Utworzenie zapytania
             Statement stm=con.createStatement();
             //stm.executeQuery("Create table test(a date, b float(7),c number(2), d varchar2(1 char))");
+            //stm.executeQuery("Create table test(picture blob)");
             LocalDateTime data=LocalDateTime.now();
             double f=4.56;
             int i=15;
             char c='a';
             String ca="ab";
-            System.out.println(f);
+            //System.out.println(f);
             //"+f+", "+i+", "+c+
-            stm.executeQuery("Insert into test values(null,"+f+","+i+",'"+c+"')");
+            PreparedStatement pstm=con.prepareStatement("Insert into test values(?)");
+            InputStream in= new FileInputStream("C:\\Users\\Xelor\\Desktop\\dot.jpg");
+            pstm.setBlob(1,in);
+            //pstm.execute();
+            //stm.executeQuery("Insert into test values("+in+")");
             ResultSet r=stm.executeQuery("Select * from test ");
             ResultSetMetaData rMeta=r.getMetaData();
-
+            //System.out.println(r);
             while(r.next()){
-                System.out.println(rMeta.getColumnName(2)+": "+r.getFloat(2));
-                System.out.println(rMeta.getColumnName(3)+": "+r.getInt(3));
-                System.out.println(rMeta.getColumnName(4)+": "+r.getString(4));
-                System.out.println("------------------");
+//                System.out.println(rMeta.getColumnName(2)+": "+r.getFloat(2));
+//                System.out.println(rMeta.getColumnName(3)+": "+r.getInt(3));
+//                System.out.println(rMeta.getColumnName(4)+": "+r.getString(4));
+//                System.out.println("------------------");
+                System.out.println(rMeta.getColumnName(1)+": "+r.getBlob(1));
             }
 
             //Krok 4: Zamknij połączenie
@@ -167,7 +212,7 @@ public class Main {
 
         }catch(Exception e){
             e.printStackTrace();
-        }
+        }*/
 
     }
 }
---
+++ src/sk/Sesja.java
@@ -0,0 +1,74 @@
+package sk;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.sql.*;
+
+import static java.lang.Thread.sleep;
+
+public class Sesja implements Runnable{
+    private Socket socket;
+
+    public Sesja(Socket socket){
+        this.socket=socket;
+    }
+
+    public void run(){
+        try{
+            //sleep(3000);
+            System.out.println("w_test");
+            PrintWriter pw=new PrintWriter(socket.getOutputStream(), true);
+            InputStreamReader isr=new InputStreamReader(socket.getInputStream());
+            BufferedReader br=new BufferedReader(isr);
+
+            //wysłanie informacji do klienta
+            //pw.println("Połączenie z serwerem zostało nawiązane.");
+            //System.out.println("przesłanie");
+
+            //odczytanie informacji od klienta
+            String zapytanie=br.readLine();
+
+            //Utworzenie połączenia do BD
+            //Załądowanie klasy drivera
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            //Utworzenie instancji połączenia
+            Connection con=DriverManager.getConnection(
+                    //jbdc:oracle:connection details //[host]:[port]/[DB service name]
+                    "jdbc:oracle:thin:@149.156.138.232:1521:orcl",
+                    "sbd01",
+                    "sbd01"
+            );
+            //Utworzenie zapytania
+            Statement stm=con.createStatement();
+
+            if(zapytanie.contains("Select")){
+                ResultSet result=stm.executeQuery(zapytanie);
+                ResultSetMetaData resultMeta =result.getMetaData();
+                //zmienna na wynik zapytania
+                String rezultat="";
+                while(result.next()){
+                    for(int i=1;i<=resultMeta.getColumnCount();i++){
+                        if(resultMeta.getColumnType(i)!=2004){
+                            rezultat+=resultMeta.getColumnName(i)+": "+result.getString(i)+";";
+                        }
+                    }
+                }
+                pw.println(rezultat);
+            }else{
+                stm.executeQuery(zapytanie);
+            }
+            //Zamknięcie połączenia
+            con.close();
+
+            pw.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
